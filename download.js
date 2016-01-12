03f06cd66a89aaa3865c8e918e400faa00b6207b
Josh Mandel <jmandel@med.harvard.edu>
2016-01-12 15:59:47 -0500

    Added download.js

---
+++ download.js
@@ -0,0 +1,17 @@
+import fs from 'fs'
+import request from 'request'
+console.log(process.argv)
+
+request(process.argv.slice(-1)[0], (err, resp, body) => {
+  console.log("Got file")
+  var wordsIn = body.split('\n')
+  var words = wordsIn
+  .map( w=> w.toLowerCase().replace(/[^a-z]/g, ""))
+  .filter( w=> w.length > 0)
+  .reduce((coll, word) => {
+    coll[word] = true
+    return coll
+  }, {})
+  var wordsSorted = Object.keys(words).sort()
+  fs.writeFileSync('dictionaty.txt', wordsSorted.join('\n'))
+})
