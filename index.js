242f00f30b4af79f5d4d855e4add9fb9595124f6
Josh Mandel <jmandel@med.harvard.edu>
2016-01-13 21:12:59 -0500

    Need to improve speed

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ try {
 console.log("Grammer built", tick())
 
 var dictionary = fs.readFileSync('dictionary/ukacd.txt').toString().split('\n')
-//dictionary = ["about", "abound", "abetter", "thousand"]
+//dictionary = ["about", "abound", "abetter", "thousand", "url", "bought", "triangle", "altering"]
 var wordExists = dictionary.reduce((coll, w) => {
   coll[w] = true
   return coll
@@ -80,175 +80,53 @@ function evaluate(start){
   return words
 }
 
-Array.prototype.flatMap = function(fn) {
-  return Array.prototype.concat.apply([], this.map(fn));
-}
-
-
-function choicesFrom(e, wordDetails){
-  var ret = []
-  var subpattern = e.pattern[0]
-  var nexte = Object.assign({}, e, {pattern: e.pattern.slice(1)})
-
-  var word = wordDetails[0]
-  var misprint = wordDetails[1]
-
-  if (typeof subpattern === "string") {
-    if (subpattern.indexOf(word[0]) !== -1){
-      ret.push([word.slice(1), misprint])
-    } else if (!misprint){
-      ret.push([word.slice(1), true])
-    }
-  } else if (subpattern[0] === 'anything'){
-    for (var i=0;i<word.length;i++){
-      ret.push([word.slice(i), misprint])
-    }
-  } else if (subpattern[0] === 'word'){
-    for (var i=2;i<word.length;i++){
-      if (wordExists[word.slice(0, i)]) {
-        ret.push([word.slice(i), misprint])
+function elementMatches(eltSpecs, anagram, context, value){
+  //console.log("Elt matchies", eltSpecs, anagram, value)
+  if (!anagram) {
+    var eltSpec = eltSpecs[0]
+    if (eltSpec.match === 'anything') return [0]
+    if (eltSpec.match === 'word') return !!wordExists[value] ? [0] : []
+    if (eltSpec.match === 'reverse-word') return !!wordExists[value.split("").reverse().join("")] ? [0] : []
+      if (typeof eltSpec === 'string'){
+        return value.length > 0 && eltSpec.indexOf(value) !== -1 ? [0] : []
       }
+    if (eltSpec.type){
+      return evaluateQualifiedPattern(eltSpec, context, value+'$') ? [0] : []
     }
-  } else if (subpattern[0] === 'reverse-word') {
-    for (var i=2;i<word.length;i++){
-      if (wordExists[word.slice(0, i).split("").reverse().join("")]) {
-        ret.push([word.slice(i), misprint])
-      }
-    }
-  }
-
-  return ret.map(r => ({nexte: nexte, word: r}))
-}
-
-function anagramChoicesFrom(e, wordDetails){
-  var ret = [];
-  var word = wordDetails[0];
-  var subpattern = e.pattern[0]
-  var nexte = Object.assign({}, e, {pattern: e.pattern.slice(1)})
-
-  if (typeof subpattern === "string") {
-    for (var i=0; i<subpattern.length; i++){
-      var char = subpattern[i];
-      var where = word.indexOf(char)
-      if (where !== -1){
-        ret.push([word.slice(0,where).concat(word.slice(where+1)), false])
-      }
-    }
-  } else if (subpattern[0] === 'anything'){
-    ret.push(['$', false])
   } else {
-    throw "unexpected character in pattern " + subpattern[0]
-  }
-
-  return ret.map(r => ({nexte: nexte, word: r}))
-}
-
-function letterbankChoicesFrom(e, wordDetails){
-  var ret = [];
-  var word = wordDetails[0];
-  var subpattern = e.pattern[0]
-  var char = word[0]
-
-
-  for (var j=0; j < e.pattern.length; j++) {
-    if (e.pattern[j][0] === 'anything'){
-      return [{nexte: e, word: ['$']}]
-    }
-    if (e.pattern[j].indexOf(char) !== -1){
-      ret.push({
-        nexte: {
-          lead: '*/',
-          pattern: e.pattern.slice(0,j).concat(e.pattern.slice(j+1))
-        },
-        word: [word.slice(1), false]
-      })
-    }
-  }
-
-  return ret
-}
-
-function cartesianProduct(paramArray) {
-
-  function addTo(curr, args) {
-
-    var i, copy, 
-        rest = args.slice(1),
-        last = !rest.length,
-        result = [];
-
-    for (i = 0; i < args[0].length; i++) {
-
-      copy = curr.slice();
-      copy.push(args[0][i]);
-
-      if (last) {
-        result.push(copy);
-
-      } else {
-        result = result.concat(addTo(copy, rest));
+    var possibleElements = []
+    for (var i=0; i < eltSpecs.length; i++){
+      var eltSpec = eltSpecs[i]
+      if (elementMatches([eltSpec], false, context, value).length > 0) {
+        possibleElements.push(i)
       }
     }
-
-    return result;
+    return possibleElements
   }
-
-  return addTo([], Array.prototype.slice.call(paramArray));
 }
 
-function* splitWordInto(word, elementLengths){
-  var lengthRanges = elementLengths.map(([lower, upper]) => {
-    var ret = []
-    for (var i=lower;i<=Math.min(upper, word.length );i++){
-      ret.push(i)
-    }
-    return ret
-  })
-  //console.log("do carteisn", elementLengths, word, lengthRanges)
-
-  var splits = combinatorics.cartesianProduct.apply(null, lengthRanges)
-  var s = splits.next()
-  while (s){
-    if (s.reduce((sum,v)=>sum+v, 0) === word.length) {
-      var substrs = []
-      var sum = 0
-      for (var i=0; i<s.length;i++) {
-        var len = s[i]
-        substrs.push(word.slice(sum, sum+len))
-        sum += len
-      }
-      yield substrs;
-    }
-    s = splits.next()
-  }
-
-}
-
-var MAX_WORD_LEN = 100
-
-
-function elementMatches(eltSpec, value){
-  //console.log("eltmatch", eltSpec, value)
-  if (eltSpec.match === 'anything') return true
-  if (eltSpec.match === 'word') return !!wordExists[value]
-  if (eltSpec.match === 'reverse-word') return !!wordExists[value.split("").reverse().join("")]
-  if (typeof eltSpec === 'string') return eltSpec.indexOf(value) !== -1
-}
-
-var patternCache = {};
-
-function rangesFit(budget, ranges, elements, word){
-
+function rangesFit(context, budget, anagram, ranges, elements, word){
   if (ranges.length === 0) {
+    //console.log("Success", word)
     return (word === '$')
   }
 
-  for (var i=ranges[0][0]; i <= Math.min(budget, ranges[0][1]);i++){
-    if (!elementMatches(elements[0], word.slice(0,i))) {
+  for (var i=0; i <= word.length;i++){
+    var possibleElements = elementMatches(elements, anagram, context, word.slice(0,i))
+    //console.log("Possible", possibleElements, elements, word, i,ranges.length, elements.length, word.slice(0, i), ".")
+
+    if (possibleElements.length == 0) {
+      //console.log("Impossible", word, i)
       continue
     }
-    if (rangesFit(budget - i, ranges.slice(1), elements.slice(1), word.slice(i))) {
-      return true
+
+    for (var j=0;j<possibleElements.length;j++) {
+      var spliceIdx = possibleElements[j]
+      var splicedRanges = ranges.slice(0,spliceIdx).concat(ranges.slice(spliceIdx+1))
+      var splicedElts = elements.slice(0,spliceIdx).concat(elements.slice(spliceIdx+1))
+      if (rangesFit(context, budget - i, anagram, splicedRanges, splicedElts, word.slice(i))) {
+        return true
+      }
     }
   }
   return false
@@ -259,34 +137,7 @@ function evaluatePatternOn(e, context, word){
   var targetLen = word.length - 1
   var elements = e.elements
   var elementLengths = e.ranges
-
-  return rangesFit(e.maxLength, e.ranges, elements, word)
-
-  var lengthRanges = elementLengths.map(([lower, upper]) => {
-    var ret = []
-    for (var i=lower;i<=Math.min(upper, word.length );i++){
-      ret.push(i)
-    }
-    return ret
-  })
-
-  var splits = splitWordInto(word.slice(0,-1), elementLengths)
-  //console.log(splits)
-  for (var split of splits) {
-    var splitWorks = true
-    for (var j=0; j<elements.length;j++){
-      if (!elementMatches(elements[j], split[j])) {
-        splitWorks = false
-        break
-      }
-    }
-    if (splitWorks){
-      return true
-    }
-  }
-
-  return false
-
+  return rangesFit(context, e.maxLength, !!e.anagram, e.ranges, elements, word)
 }
 
 function evaluateSimplePattern(e, context, word){
